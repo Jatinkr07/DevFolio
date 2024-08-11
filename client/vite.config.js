d3cb0bb6c19deffa4dca70525c2cbe6c78378d5c
Jatinkr07 <jatinkumar07911@gmail.com>
2024-08-11 20:02:02 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      input: "./src/main.jsx", // Make sure this points to your actual entry file
+    },
+  },
+  optimizeDeps: {
+    include: ["some-package", "another-package"],
+  },
 });
