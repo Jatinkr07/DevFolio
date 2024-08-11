4ecdfc5756db4d6877485a3fc0d08f31464e676d
Jatinkr07 <jatinkumar07911@gmail.com>
2024-08-11 19:49:10 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin:
-      "https://66b8c66aa5564d59252675d8--delicate-frangollo-362922.netlify.app",
+    origin: "http://localhost:5173",
   })
 );
 app.use(express.json());
