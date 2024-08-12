1c4a110b9c5e6610e64289c8eff7de877a610f3a
Jatinkr07 <jatinkumar07911@gmail.com>
2024-08-12 10:39:46 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://jatinkumar-devfoliov1.onrender.com",
   })
 );
 app.use(express.json());
